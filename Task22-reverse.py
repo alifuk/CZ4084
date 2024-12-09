b37663b947ea483ea85a91ee75f29288f1d85f28
AlbertA <albert.mysak@gmail.com>
2024-12-09 20:10:46 +0100

    solution 22

--- Task22-reverse.py
+++ Task22-reverse.py
@@ -1,5 +1,11 @@
 puvodni_list = [1,4,9,[5,2],6] #Nesahat na toto
 # Použijte dva příkazy .reverse() tak ať dostanete: [6, [2, 5], 9, 4, 1]
+#Možnost1
+#puvodni_list.reverse()
+#puvodni_list[1].reverse()
+#Možnost2
+puvodni_list[3].reverse()
+puvodni_list.reverse()
 
 
 #Na řádky níže nesahat
