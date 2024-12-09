ceea459c9dbac4e9bcda347288202ffea584271e
AlbertA <albert.mysak@gmail.com>
2024-12-09 19:05:57 +0100

    task 20 a 21

---
+++ Task20.py
@@ -0,0 +1,9 @@
+vsechny_jmena = ["Jura", ["Eliška", "Ruda"], "Božka", "Katka", ["Michal", "Liza"]] #Tento řádek neupravujte
+#pomocí příkazu .remove(x) odeberte vsechny_jmena, tak aby obsahovala pouze:
+# ["Jura", ["Eliška"], "Katka"]
+
+
+#   ŘÁDKY níže neupravujte
+jmena = ["Jura", ["Eliška"], "Katka"]
+assert jmena==vsechny_jmena, f"Chyba, vyšlo {vsechny_jmena}, ale mělo vyjít {jmena} "
+print("Gratuluji")
---
+++ Task21-clear.py
@@ -0,0 +1,8 @@
+vsechny_jmena = ["Jura", ["Eliška", "Ruda"], "Božka", "Katka", ["Michal", "Liza"]] #Tento řádek neupravujte
+#pomocí příkazu .clean() promažte vnořené listy tak, aby vsechny_jmena obsahovala pouze:
+# ["Jura", [], "Božka", "Katka", []]
+
+#   ŘÁDKY níže neupravujte
+jmena = ["Jura", [], "Božka", "Katka", []]
+assert jmena==vsechny_jmena, f"Chyba, vyšlo {vsechny_jmena}, ale mělo vyjít {jmena} "
+print("Gratuluji")
