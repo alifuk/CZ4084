12e871c0aa6bdaed8ac52c6bbe8a2eb15041220f
AlbertA <albert.mysak@gmail.com>
2024-12-09 18:40:07 +0100

    task 18

---
+++ Task18.py
@@ -0,0 +1,12 @@
+jmena = ["Jura", ["Eliška"], "Katka"] #Tento řádek neupravujte
+#pomocí příkazu .insert() rozšiřte jmena, tak aby obsahovala:
+# ["Jura", ["Eliška", "Ruda"], "Božka", "Katka", ["Michal", "Liza"]]
+
+
+#   ŘÁDKY níže neupravujte
+vsechny_jmena = ["Jura", ["Eliška", "Ruda"], "Božka", "Katka", ["Michal", "Liza"]]
+assert jmena==vsechny_jmena, f"Chyba, vyšlo {jmena}, ale mělo vyjít {vsechny_jmena} "
+print("Gratuluji")
+
+
+
