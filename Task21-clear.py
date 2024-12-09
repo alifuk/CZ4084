1fd81e31ef2beed85aa5221227a10547a660acd3
AlbertA <albert.mysak@gmail.com>
2024-12-09 20:05:17 +0100

    solution 21

--- Task21-clear.py
+++ Task21-clear.py
@@ -1,7 +1,8 @@
 vsechny_jmena = ["Jura", ["Eliška", "Ruda"], "Božka", "Katka", ["Michal", "Liza"]] #Tento řádek neupravujte
-#pomocí příkazu .clean() promažte vnořené listy tak, aby vsechny_jmena obsahovala pouze:
+#pomocí příkazu .clear() promažte vnořené listy tak, aby vsechny_jmena obsahovala pouze:
 # ["Jura", [], "Božka", "Katka", []]
-
+vsechny_jmena[1].clear()
+vsechny_jmena[4].clear()
 #   ŘÁDKY níže neupravujte
 jmena = ["Jura", [], "Božka", "Katka", []]
 assert jmena==vsechny_jmena, f"Chyba, vyšlo {vsechny_jmena}, ale mělo vyjít {jmena} "
