4181d339afcfb739fb9319eab68a3b8a25e12a66
AlbertA <albert.mysak@gmail.com>
2024-12-10 17:57:19 +0100

    Task 26

---
+++ Task26.py
@@ -0,0 +1,14 @@
+contacts = {
+    "Alice": {"email": "alice@example.com", "phone": "555-1234"},
+    "Bob": {"email": "bob@example.com", "phone": "555-5678"},
+    "Charlie": {"email": "charlie@example.com", "phone": "555-9101"},
+    "Diana": {"email": "diana@example.com", "phone": "555-3456"}
+}
+#pomocí .get() získejte telefon na Charlieho. Pokud telefon neexistuje, vypište
+#'Číslo není v záznamu'
+
+#smažte "Charlie" z kontaktů
+
+#použijte úplně stejný kód jako na řádku 9. Program nesmí vypsat chybu
+
+#Dianě smažte telefon, ale email jí ponechejte
