d12332938ef5f0264c718f1972852f00d4968197
AlbertA <albert.mysak@gmail.com>
2024-12-10 19:19:53 +0100

    Task 29

---
+++ Example28-input.py
@@ -0,0 +1,6 @@
+import sys
+
+soucet = int(sys.argv[1]) + int(sys.argv[2])
+print(f"Součet je: {soucet}")
+
+print(type(sys.argv[1]))
---
+++ Task29-unique.py
@@ -0,0 +1,6 @@
+# pro vstup Shrek Transformers WhiteChrismas
+print("Shrek Transformers WhiteChrismas")
+# pro vstup Shrek Shrek Shrek
+print("Shrek")
+# pro vstup 5 3 3
+print("5 3")
