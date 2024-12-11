d791918bceb01928515103aaed492275294dbe40
AlbertA <albert.mysak@gmail.com>
2024-12-11 20:54:42 +0100

    38 krok po kroku

---
+++ Example38-krok_po_kroku.py
@@ -0,0 +1,26 @@
+cislo = 1
+while cislo < 5:
+    print(cislo)
+    cislo += 1
+print(f"Hotovo, číslo je {cislo}")
+#vykoná se řádek 1 -> cislo bude 1
+#vykoná se porovnání na řádku 2. 1 < 5, takže se začne vykonávat tělo while
+#vykoná se řádek 3 -> vypíše se 1
+#vykoná se řádek 4 -> cislo bude 2
+#vykoná se porovnání na řádku 2. 2 < 5, takže se začne vykonávat tělo while
+#vykoná se řádek 3 -> vypíše se 2
+#vykoná se řádek 4 -> cislo bude 3
+#vykoná se porovnání na řádku 2. 3 < 5, takže se začne vykonávat tělo while
+#vykoná se řádek 3 -> vypíše se 3
+#vykoná se řádek 4 -> cislo bude 4
+#vykoná se porovnání na řádku 2. 4 < 5, takže se začne vykonávat tělo while
+#vykoná se řádek 3 -> vypíše se 4
+#vykoná se řádek 4 -> cislo bude 5
+#vykoná se porovnání na řádku 2. 5 < 5 NEPLATÍ, takže se skočí na řádek 5
+#vykoná se řádek 5 - vypíše se Hotovo, číslo je 5
+
+
+
+
+
+
