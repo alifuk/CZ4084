1bc12c82b11232b94e5770fc8ff01b4924a37761
AlbertA <albert.mysak@gmail.com>
2024-12-12 19:23:55 +0100

    Task 44

---
+++ Task44-velka_faktura.py
@@ -0,0 +1,9 @@
+faktury_k_vystaveni = [
+    ["Panasonic", 30000, "Martin Novotný"],
+    ["Oracle", 22000, "Petr Lukoš"],
+    ["Porsche", 32345, "Ivo Gira"],
+    ["Siemens", 2000, "Luboš Šejk"],
+]
+ico_moji_firmy = 3456785
+#Vypište
+#Faktura ID 0. Vystavuje firma 3456785. Faktura pro Martin Novotný na částku 30000 pro firmu Panasonic
