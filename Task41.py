5061346c506c780eb6c323f9b518993027b1d0c2
AlbertA <albert.mysak@gmail.com>
2024-12-12 18:18:37 +0100

    task 41

---
+++ Task41.py
@@ -0,0 +1,10 @@
+#Úkoly
+#Vypište čísla 0-5 pomocí for cyklu a funkce range()
+
+#Vypište čísla 40-46 pomocí for cyklu a funkce range()
+
+#Vypište čísla 10,20,30,...,80 pomocí for cyklu a funkce range()
+
+#Vypište čísla od -4 do 2pomocí for cyklu a funkce range()
+
+
