20019d2579328541240e4d392889770421aa8074
AlbertA <albert.mysak@gmail.com>
2024-12-12 18:38:35 +0100

    solution 41

--- Task41.py
+++ Task41.py
@@ -1,10 +1,16 @@
 #Úkoly
 #Vypište čísla 0-5 pomocí for cyklu a funkce range()
-
+for a in range(6):
+    print(a)
 #Vypište čísla 40-46 pomocí for cyklu a funkce range()
-
+for b in range(40,47):
+    print(b)
 #Vypište čísla 10,20,30,...,80 pomocí for cyklu a funkce range()
-
+for c in range(10,90,10):
+    print(c)
 #Vypište čísla od -4 do 2pomocí for cyklu a funkce range()
-
-
+for d in range(-4, 3):
+    print(d)
+#Jak vypsat čísla dolu od 8 do 4
+for e in range(8,3,-1):
+    print(e)
