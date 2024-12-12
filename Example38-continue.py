0778dc896929c83a5a5caab06ca85af04116afc1
AlbertA <albert.mysak@gmail.com>
2024-12-12 17:50:36 +0100

    39 task a example

---
+++ Example38-continue.py
@@ -0,0 +1,20 @@
+
+'''
+n = 0
+soucet_lichych_cisel = 0
+while n < 7:
+    n += 1
+    if n % 2 == 0:
+        print("N je sudé")
+        continue
+    print(f"Hodnota N je {n}")
+    soucet_lichych_cisel += n
+print(soucet_lichych_cisel)
+'''
+
+
+cisla = [3, 4, 5, 20, 30]
+for cislo in cisla:
+    print(cislo*2)
+
+
---
+++ Task39.py
@@ -0,0 +1,15 @@
+zvirata = ["Kočka", "Pes", "Had"]
+#TADY NAPSAT FORCYKLUS KTERÝ POSTUPNĚ VYPÍŠE
+#"Kočka běhá"
+#"Pes běhá"
+#"Had běhá"
+
+
+
+
+#Druhá část úkolu
+stari = [["Žirafa", 20], ["Slon", 40], ["Dikobraz", 5]]
+#NAPSAT FORCYKLUS PŘES stari. Pro každý prvek (list) vypíšete:
+#Žirafa má věk 20
+#Slon má věk 40
+#Dikobraz má věk 5
