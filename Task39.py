e002ab6c02ede97589e02b8d60857378bcececcb
AlbertA <albert.mysak@gmail.com>
2024-12-12 18:02:29 +0100

    39 task solution

--- Task39.py
+++ Task39.py
@@ -1,11 +1,10 @@
-zvirata = ["Kočka", "Pes", "Had"]
+zvirata = ["Kočka", "Pes", "Had", "Moucha", "Pavouk"]
 #TADY NAPSAT FORCYKLUS KTERÝ POSTUPNĚ VYPÍŠE
 #"Kočka běhá"
 #"Pes běhá"
 #"Had běhá"
-
-
-
+for zvire in zvirata:
+    print(f"{zvire} běhá")
 
 #Druhá část úkolu
 stari = [["Žirafa", 20], ["Slon", 40], ["Dikobraz", 5]]
@@ -13,3 +12,8 @@ stari = [["Žirafa", 20], ["Slon", 40], ["Dikobraz", 5]]
 #Žirafa má věk 20
 #Slon má věk 40
 #Dikobraz má věk 5
+for info_o_zvireti in stari:
+    print(info_o_zvireti)
+    print(f"{info_o_zvireti[0]} má věk {info_o_zvireti[1]}")
+
+
